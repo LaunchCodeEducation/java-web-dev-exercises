93c1650e5621b796209a6793a6f07a5f69fa10e9
Carly Langlois <cj.langlois.cl@gmail.com>
2019-09-20 20:32:43 -0500

    remove period!

--- src/org/launchcode/java/demos/Main.java
+++
@@ -1,8 +0,0 @@
-package org.launchcode.java.demos;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- src/org/launchcode/java/demos/javawebdevelopment/Hello.java
+++ src/org/launchcode/java/demos/javawebdevelopment/Hello.java
@@ -1,4 +1,4 @@
-package org.launchcode.java.demos.javawebdevelopment.;
+package org.launchcode.java.demos.javawebdevelopment;
 
 /**
  * From "Java Web Development"
