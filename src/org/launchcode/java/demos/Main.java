fb18c34990a8e85c17b6b5e4c1fa5875f3bcedeb
Carly Langlois <cj.langlois.cl@gmail.com>
2019-09-20 20:23:50 -0500

    first commit with Hello class

---
+++ src/org/launchcode/java/demos/Main.java
@@ -0,0 +1,8 @@
+package org.launchcode.java.demos;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/org/launchcode/java/demos/javawebdevelopment/Hello.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.demos.javawebdevelopment.;
+
+/**
+ * From "Java Web Development"
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
