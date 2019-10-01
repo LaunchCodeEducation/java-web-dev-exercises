11fc33da87eca3d5b3048488165dfbbd7a4d145c
jimflores <jflores@launchcode.org>
2019-10-01 12:07:10 -0500

    Added HelloMethods demo code to lesson 1.

---
+++ src/org/launchcode/java/demos/HelloMethods.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.demos;
+
+public class HelloMethods {
+
+    public static void main(String[] args) {
+        String message = Message.getMessage("fr");
+        System.out.println(message);
+    }
+
+}
---
+++ src/org/launchcode/java/demos/Message.java
@@ -0,0 +1,15 @@
+package org.launchcode.java.demos;
+
+public class Message {
+
+    public static String getMessage(String lang) {
+
+        if (lang.equals("sp")) {
+            return "¡Hola, Mundo!";
+        } else if (lang.equals("fr")) {
+            return "Bonjour, le monde!";
+        } else {
+            return "Hello, World!";
+        }
+    }
+}
