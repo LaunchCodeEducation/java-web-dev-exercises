8d4ca769af89d77e5667efafa428aa4a3375a766
jimflores <jflores@launchcode.org>
2019-10-01 11:56:55 -0500

    Added TempConverter program to lesson 1 demo code.

---
+++ src/org/launchcode/java/demos/TempConverter.java
@@ -0,0 +1,19 @@
+package org.launchcode.java.demos;
+
+import java.util.Scanner;
+
+public class TempConverter {
+    public static void main(String[] args) {
+        double fahrenheit;
+        double celsius;
+        Scanner input;
+
+        input = new Scanner(System.in);
+        System.out.println("Enter the temperature in Fahrenheit: ");
+        fahrenheit = input.nextDouble();
+        input.close();
+
+        celsius = (fahrenheit - 32) * 5/9;
+        System.out.println("The temperature in Celsius is: " + celsius + "°C");
+    }
+}
