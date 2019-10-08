4015c82d8e4e1f7569c25dd90a4a3de006cdc42c
Carly Langlois <cj.langlois.cl@gmail.com>
2019-10-08 16:37:13 -0500

    add hashmap gradebook example

---
+++ src/org/launchcode/java/demos/collections/HashMapGradebook.java
@@ -0,0 +1,45 @@
+package org.launchcode.java.demos.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HashMapGradebook {
+    public static void main(String[] args) {
+
+        HashMap<String, Double> students = new HashMap<>();
+        Scanner in = new Scanner(System.in);
+        String newStudent;
+
+        System.out.println("Enter your students (or ENTER to finish):");
+
+        // Get student names and grades
+        do {
+
+            System.out.print("Student: ");
+            newStudent = in.nextLine();
+
+            if (!newStudent.equals("")) {
+                System.out.print("Grade: ");
+                Double newGrade = in.nextDouble();
+                students.put(newStudent, newGrade);
+
+                // Read in the newline before looping back
+                in.nextLine();
+            }
+
+        } while(!newStudent.equals(""));
+
+        // Print class roster
+        System.out.println("\nClass roster:");
+        double sum = 0.0;
+
+        for (Map.Entry<String, Double> student : students.entrySet()) {
+            System.out.println(student.getKey() + " (" + student.getValue() + ")");
+            sum += student.getValue();
+        }
+
+        double avg = sum / students.size();
+        System.out.println("Average grade: " + avg);
+    }
+}
