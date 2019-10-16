b4f1b35038dcb47a998522ad474609be82fd8b3d
Sally Steuterman <sally.steuterman@gmail.com>
2019-10-16 13:34:06 -0500

    Adding Student class

---
+++ src/org/launchcode/java/demos/school/Student.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.demos.school;
+
+public class Student {
+
+    private String name;
+    private int studentId;
+    private int numberOfCredits = 0;
+    private double gpa = 0.0;
+
+}
