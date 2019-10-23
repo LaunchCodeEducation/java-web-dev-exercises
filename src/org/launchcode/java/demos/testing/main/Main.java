8363e5504182df8622fb234e9eec8732b9fc823a
Carly Langlois <cj.langlois.cl@gmail.com>
2019-10-23 12:44:08 -0500

    add lib and testing demo starter code

---
+++ src/org/launchcode/java/demos/testing/main/Main.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.demos.testing.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car("Toyota", "Prius", 10, 50);
+        System.out.println(car.getMake() + " - " + car.getModel());
+    }
+}
---
+++ src/org/launchcode/java/demos/testing/test/CarTest.java
@@ -0,0 +1,4 @@
+package org.launchcode.java.demos.testing.test;
+
+public class CarTest {
+}
