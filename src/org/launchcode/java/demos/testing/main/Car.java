c493e4bb163c0e127e0a99d933c9a2e5fdd52913
Carly Langlois <cj.langlois.cl@gmail.com>
2019-10-23 15:23:24 -0500

    remove solution code remaining

--- src/org/launchcode/java/demos/testing/main/Car.java
+++ src/org/launchcode/java/demos/testing/main/Car.java
@@ -82,7 +82,4 @@ public class Car {
         this.odometer += milesAbleToTravel;
     }
 
-    public void addGas(double gas) {
-        this.setGasTankLevel(gas + this.getGasTankLevel());
-    }
 }
--- src/org/launchcode/java/demos/testing/test/CarTest.java
+++ src/org/launchcode/java/demos/testing/test/CarTest.java
@@ -1,4 +1,11 @@
 package org.launchcode.java.demos.testing.test;
 
 public class CarTest {
+
+    //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
+    //TODO: constructor sets gasTankLevel properly
+    //TODO: gasTankLevel is accurate after driving within tank range
+    //TODO: gasTankLevel is accurate after attempting to drive past tank range
+    //TODO: can't have more gas than tank size, expect an exception
+
 }
