22ce9919e0ce190ebff5bed23973ef3287616755
Sally Steuterman <sally.steuterman@gmail.com>
2019-10-29 15:23:52 -0500

    Adding Lesson 6 starter code

---
+++ src/org/launchcode/java/demos/technology/Computer.java
@@ -0,0 +1,5 @@
+package org.launchcode.java.demos.technology;
+
+public class Computer {
+    // Write the code for your Computer class here!
+}
---
+++ src/org/launchcode/java/demos/technology/Program.java
@@ -0,0 +1,5 @@
+package org.launchcode.java.demos.technology;
+
+public class Program {
+    // Test out your new classes here!
+}
