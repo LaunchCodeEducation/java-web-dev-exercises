13570eb38e77a152d0a97e75d6be430f6c835943
jimflores <jflores@launchcode.org>
2019-11-08 08:50:34 -0600

    Fixed allergen assignments in lesson 7 starter code.

--- src/org/launchcode/java/demos/lsn7interfaces/Case.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Case.java
@@ -19,8 +19,8 @@ public class Case {
 
         Flavor flavor1 = new Flavor("Vanilla", 0.75, new ArrayList<>(Arrays.asList( "dairy")));
         Flavor flavor2 = new Flavor("Chocolate", 0.75, new ArrayList<>(Arrays.asList( "dairy")));
-        Flavor flavor3 = new Flavor("Red Velvet", 0.85, new ArrayList<>(Arrays.asList( "dairy, red #5")));
-        Flavor flavor4 = new Flavor("Rocky Road", 0.85, new ArrayList<>(Arrays.asList( "dairy, nuts, gelatin")));
+        Flavor flavor3 = new Flavor("Red Velvet", 0.85, new ArrayList<>(Arrays.asList( "dairy", "red #5")));
+        Flavor flavor4 = new Flavor("Rocky Road", 0.85, new ArrayList<>(Arrays.asList( "dairy", "nuts", "gelatin")));
         Flavor flavor5 = new Flavor("Strawberry Sorbet", 0.50, new ArrayList<>(Arrays.asList( "strawberry")));
         flavors.add(flavor1);
         flavors.add(flavor2);
