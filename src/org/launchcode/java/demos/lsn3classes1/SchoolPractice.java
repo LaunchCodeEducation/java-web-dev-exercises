2e91c0ec130e34368c0c2d6071d4b18de1a70d38
Kisha Codes <Kishacodes@gmail.com>
2020-06-28 16:06:03 -0400

    Completed 2 out of 3 Class 5 exercises

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,11 +1,18 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class SchoolPractice {
     public static void main(String[] args) {
         // 2. Instantiate your Student class for part 2 here!
-        Student kishaHymes = new Student();
-        kishaHymes.setName("Kisha Hymes");
-        kishaHymes.setNumberOfCredits(1);
-        kishaHymes.setGpa(4.0);
+        Student kishaHymes = new Student("Kisha Hymes", 0, 1, 4.0);
+//        kishaHymes.setName("Kisha Hymes");
+//        kishaHymes.setNumberOfCredits(1);
+//        kishaHymes.setGpa(4.0);
+
+        kishaHymes.addGrade(3,3.0); //testing methods to see if they work correctly.
+        System.out.println(kishaHymes.studentInfo());
+        System.out.println(kishaHymes.getGradeLevel());
     }
+
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -4,18 +4,62 @@ package org.launchcode.java.demos.lsn3classes1;
 // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
 public class Student {
-
+    private static int nextStudentId = 1;
     private String name;
     private int studentId;
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
-    //1. Add necessary getters and setters
+    //5.1 Add constructor
+    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this(name, studentId, 0,0);
+    }
+    public Student(String name) {
+        this(name, nextStudentId);
+        nextStudentId++;
+    }
+
+    public String studentInfo() {
+        return (this.name + " has a GPA of: " + this.gpa);
+    }
+
+    public void addGrade(int courseCredits, double grade) {
+        //5.2 update the fields for numberofCredits, gpa
+        this.numberOfCredits = this.numberOfCredits + courseCredits;
+        this.gpa = ((this.gpa + (grade * courseCredits))/this.numberOfCredits);
+        setNumberOfCredits(this.numberOfCredits);
+        setGpa(this.gpa);
+
+    }
+
+    public String getGradeLevel() {
+        //5.1 determine the grade level based on numberofCredits
+        if (this.numberOfCredits < 30) {
+            return (this.name + " has " + numberOfCredits + " credits and is a Freshman.");
+        }
+        else if (this.numberOfCredits < 60) {
+            return (this.name + " has " + numberOfCredits + " credits and is a Sophomore.");
+        }
+        else if (this.numberOfCredits < 90) {
+            return (this.name + " has " + numberOfCredits + " credits and is a Junior.");
+        }
+        else {
+            return (this.name + " has " + numberOfCredits + " credits and is a Senior.");
+        }
+    }
+
+    //4.1. Add necessary getters and setters
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -23,7 +67,6 @@ public class Student {
     public int getNumberOfCredits() {
         return numberOfCredits;
     }
-
     public void setNumberOfCredits(int numberOfCredits) {
         this.numberOfCredits = numberOfCredits;
     }
@@ -31,7 +74,6 @@ public class Student {
     public double getGpa() {
         return gpa;
     }
-
     public void setGpa(double gpa) {
         this.gpa = gpa;
     }
