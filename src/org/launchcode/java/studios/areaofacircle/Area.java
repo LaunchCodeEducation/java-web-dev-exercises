d210b99ca256b7d14371096f05fb9958e7534300
May Lin <40640630+maymlin@users.noreply.github.com>
2021-08-13 17:24:23 -0500

    Complete 2.5.3 Studio next task and 2.5.4 Bonus Missions

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,15 +1,27 @@
 package org.launchcode.java.studios.areaofacircle;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        double radius = -1;
+        double areaOfCircle;
 
-        System.out.print("Enter a radius: ");
-        double radius = input.nextDouble();
-        double areaOfCircle = Math.pow(radius, 2) * Math.PI;
+//        https://kodejava.org/how-do-i-validate-input-when-using-scanner/
+        do {
+            System.out.print("Enter a radius: ");
+            while (!input.hasNextDouble()) {
+                String invalidRadius = input.next();
+                System.out.println("'" + invalidRadius + "'" + " is not a valid input.\n");
+                System.out.print("Enter a radius: ");
+            }
+            radius = input.nextDouble();
+        } while (radius <= 0);
+
+        areaOfCircle = Circle.getArea(radius);
         areaOfCircle = Math.round(areaOfCircle * 100.0) / 100.0;
-        System.out.println("The are of a circle of radius " + radius + " is:" + areaOfCircle);
+        System.out.println("The are of a circle of radius " + radius + " is: " + areaOfCircle);
     }
 }
---
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,7 @@
+package org.launchcode.java.studios.areaofacircle;
+
+public class Circle {
+    public static Double getArea(Double radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
