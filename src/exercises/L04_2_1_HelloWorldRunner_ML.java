e008fe6d8e840f98c60504b2ba12f65b8fadac64
May Lin <40640630+maymlin@users.noreply.github.com>
2021-08-17 16:00:10 -0500

    Complete 4.2 Modifiers in Java

---
+++ src/exercises/L04_2_1_HelloWorldRunner_ML.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class L04_2_1_HelloWorldRunner_ML {
+    public static void main(String[] args) {
+        L04_2_1_HelloWorld_ML hello = new L04_2_1_HelloWorld_ML();
+        hello.sayHello();
+        hello.sayNight();
+    }
+}
--- src/exercises/L04_2_1_HelloWorld_ML.java
+++ src/exercises/L04_2_1_HelloWorld_ML.java
@@ -1,5 +1,26 @@
 package exercises;
 
 public class L04_2_1_HelloWorld_ML {
+    String message = "Hello World";     // removed public from
+                                        // L04_1_1_HelloWorld_ML declaration
+
+    void sayHello() {
+        System.out.println(message);    // also removed public from declaration
+    }
+
+    // 4.2.1 Access Modifiers
+    // In this HelloWorld class, we omit the public access modifier in
+    // lines 3 and 5. Doing this implicitly gives the message field and
+    // the sayHello method default access.
+
+    // We should avoid giving everything default access when creating classes
+    // in Java and instead think carefully about what level of access each
+    // field and method should have.
+
+    private String newMessage = "Good night, World";
+
+    public void sayNight() {
+        System.out.println(newMessage);
+    }
 
 }
