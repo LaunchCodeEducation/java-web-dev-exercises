3db0b8d03afdcc9df0865a3583e9ffcc18a69e90
jespinol <76534045+jespinol@users.noreply.github.com>
2021-09-20 19:55:16 -0400

    Added some validation function

--- src/org/launchcode/java/studios/areaofcircle/Area.java
+++ src/org/launchcode/java/studios/areaofcircle/Area.java
@@ -6,10 +6,16 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius:");
-        Double radius = input.nextDouble();
-//        System.out.println("initialize radius");
-        Double area = Circle.getArea(radius);
-//        System.out.println("get area");
-        System.out.println("The area of a circle of radius " + radius + " is: " + area);
+        try {
+            Double radius = input.nextDouble();
+            if (radius > 0) {
+                Double area = Circle.getArea(radius);
+                System.out.println("The area of a circle of radius " + radius + " is: " + area);
+            } else {
+                System.out.println("Cannot use a negative radius");
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid radius");
+        }
     }
 }
