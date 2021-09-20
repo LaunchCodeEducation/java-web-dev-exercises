06d9cc6c049ad7608b22ff80ef2d0132323a8f52
jespinol <76534045+jespinol@users.noreply.github.com>
2021-09-20 19:59:35 -0400

    Improved validation. Added while loop. Validation still doesn't handle empty strings well.

--- src/org/launchcode/java/studios/areaofcircle/Area.java
+++ src/org/launchcode/java/studios/areaofcircle/Area.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a radius:");
-        try {
-            Double radius = input.nextDouble();
-            if (radius > 0) {
-                Double area = Circle.getArea(radius);
-                System.out.println("The area of a circle of radius " + radius + " is: " + area);
-            } else {
-                System.out.println("Cannot use a negative radius");
+        Double radius = -1.00;
+        while (radius < 0) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter a radius:");
+            try {
+                radius = input.nextDouble();
+                if (radius > 0) {
+                    Double area = Circle.getArea(radius);
+                    System.out.println("The area of a circle of radius " + radius + " is: " + area);
+                } else {
+                    System.out.println("Cannot use a negative radius");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid radius");
             }
-        } catch (Exception e) {
-            System.out.println("Invalid radius");
         }
     }
 }
