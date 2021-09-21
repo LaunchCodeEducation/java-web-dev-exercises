d56c41e83a59c17408dde2c31dc83986ec3eb2fa
jespinol <76534045+jespinol@users.noreply.github.com>
2021-09-20 20:42:47 -0400

    Made loop logic more readable. Changed some error messages. Made some type changes suggested by IDE.

--- src/org/launchcode/java/studios/areaofcircle/Area.java
+++ src/org/launchcode/java/studios/areaofcircle/Area.java
@@ -4,20 +4,21 @@ import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
-        Double radius = -1.00;
-        while (radius < 0) {
+        boolean validRadius = false;
+        while (!validRadius) {
             Scanner input = new Scanner(System.in);
             System.out.println("Enter a radius:");
             try {
-                radius = input.nextDouble();
+                double radius = input.nextDouble();
                 if (radius > 0) {
                     Double area = Circle.getArea(radius);
                     System.out.println("The area of a circle of radius " + radius + " is: " + area);
+                    validRadius = true;
                 } else {
                     System.out.println("Cannot use a negative radius");
                 }
             } catch (Exception e) {
-                System.out.println("Invalid radius");
+                System.out.println("Invalid radius, must be a positive number");
             }
         }
     }
