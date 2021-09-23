f63ad58df7e8feb269b9a5dfc6ccbc93223173c8
jespinol <76534045+jespinol@users.noreply.github.com>
2021-09-23 17:56:25 -0400

    Completed additional parts.

--- src/exercises2/ArrayListPractice.java
+++ src/exercises2/ArrayListPractice.java
@@ -1,8 +1,18 @@
 package exercises2;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class ArrayListPractice {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i =1; i <11; i++) {
+            numbers.add(i);
+        }
+        Integer sumOfEven = sumEvenNum(numbers);
+        System.out.println(sumOfEven);
+    }
 
     public static int sumEvenNum(ArrayList<Integer> arr) {
         int sum = 0;
@@ -13,4 +23,5 @@ public class ArrayListPractice {
         }
         return sum;
     }
+
 }
