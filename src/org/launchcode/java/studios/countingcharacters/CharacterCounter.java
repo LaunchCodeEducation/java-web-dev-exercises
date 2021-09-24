2030880b52292d2b1463e6ba2aa8010e6530f9ba
jespinol <76534045+jespinol@users.noreply.github.com>
2021-09-23 20:35:30 -0400

    Improved logic in several parts.

--- src/org/launchcode/java/studios/countingcharacters/CharacterCounter.java
+++ src/org/launchcode/java/studios/countingcharacters/CharacterCounter.java
@@ -4,26 +4,27 @@ import java.util.*;
 
 public class CharacterCounter {
 
-    public static ArrayList<String> toArray(String input) {
+    public static ArrayList<Character> toArray(String input) {
         String userInput = input;
         // set a default string if the user doesn't provide an input string
-        if (input.length() < 1) {
+        if (input.trim().isEmpty()) {
             userInput = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
-            System.out.println("No input, using: " + userInput);
+            System.out.println("No input, using default string: " + userInput);
         }
-        char[] inputArr = userInput.toCharArray();
-        ArrayList<String> charsInInput = new ArrayList<>();
+        String userInputLower = userInput.toLowerCase();
+        char[] inputArr = userInputLower.toCharArray();
+        ArrayList<Character> charsInInput = new ArrayList<>();
         for (char c : inputArr) {
-            if ((c >= 'A' && c <= 'z')) {
-                charsInInput.add(String.valueOf(c).toLowerCase());
+            if ((c >= 'a' && c <= 'z')) {
+                charsInInput.add(c);
             }
         }
         return charsInInput;
     }
 
-    public static HashMap<String, Integer> countChars(ArrayList<String> input) {
-        HashMap<String, Integer> counts = new HashMap<>();
-        for (String c : input) {
+    public static HashMap<Character, Integer> countChars(ArrayList<Character> input) {
+        HashMap<Character, Integer> counts = new HashMap<>();
+        for (Character c : input) {
             if (counts.containsKey(c)) {
                 counts.replace(c, counts.get(c) + 1);
             } else {
@@ -33,8 +34,11 @@ public class CharacterCounter {
         return counts;
     }
 
-    public static void printChars(HashMap<String, Integer> input) {
-        for (Map.Entry<String, Integer> character : input.entrySet()) {
+    public static void printChars(HashMap<Character, Integer> input) {
+        if (input.size() == 0) {
+            System.out.println("Only non-alphabetic characters were provided :(");
+        }
+        for (Map.Entry<Character, Integer> character : input.entrySet()) {
             System.out.println(character.getKey() + ": " + character.getValue());
         }
     }
@@ -43,8 +47,8 @@ public class CharacterCounter {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a string: ");
         String userInput = input.nextLine();
-        ArrayList<String> characterArray = toArray(userInput);
-        HashMap<String, Integer> uniqueCounts = countChars(characterArray);
+        ArrayList<Character> characterArray = toArray(userInput);
+        HashMap<Character, Integer> uniqueCounts = countChars(characterArray);
         printChars(uniqueCounts);
     }
 }
