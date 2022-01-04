37befe69f63bfaf89e6ead0a62a8e80c77da5fde
Natasha Matthews <natashamatthews90@gmail.com>
2022-01-03 21:28:48 -0600

    added a do while loop

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -4,10 +4,27 @@ import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        /*Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
         Double radius = input.nextDouble();
         Double area = Circle.getArea(radius);
-        System.out.println("The area of a circle of radius " + radius + " is :" + area);
+        System.out.println("The area of a circle of radius " + radius + " is :" + area); */
+
+        Scanner input = new Scanner(System.in);
+        Double radius;
+        do {
+            System.out.println("Please enter the radius of the circle in cm: ");
+            while (!input.hasNextDouble()) {
+                System.out.println("Please enter a valid radius.");
+                input.next();
+            }
+            radius = input.nextDouble();
+        } while (radius < 0);
+        input.close();
+        Double area = Circle.getArea(radius);
+
+
+        System.out.println("The area of a circle with a radius of " + radius +"cm is " + area + " centimeters squared.");
+
     }
 }
