909829de44586f8509c07a57adfe5f951c7ff519
Natasha Matthews <natashamatthews90@gmail.com>
2022-01-04 19:23:06 -0600

    added the sumEven method

--- src/exercises/Array.java
+++ src/exercises/Array.java
@@ -1,10 +1,26 @@
 package exercises;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Array {
     public static void main(String[] args) {
         int[] numbersArray = { 1, 1, 2, 3, 5, 8};
+        ArrayList<Integer> numbersList = new ArrayList<>();
+
+        numbersList.add(1);
+        numbersList.add(2);
+        numbersList.add(3);
+        numbersList.add(4);
+        numbersList.add(5);
+        numbersList.add(6);
+        numbersList.add(7);
+        numbersList.add(8);
+        numbersList.add(9);
+        numbersList.add(10);
+        int sumOfAllEvens = sumEven(numbersList);
+        System.out.println(sumOfAllEvens);
+
 
         for (int i : numbersArray) {
             if ( i % 2 != 0 ) {
@@ -17,10 +33,21 @@ public class Array {
 
             String[] sentences = phrase.split("\\.");
             System.out.println(Arrays.toString(sentences));
-
         }
 
     }
+
+
+    public static int sumEven(ArrayList<Integer> arr) {
+        int total = 0;
+        for (int integer : arr) {
+            if (integer % 2 == 0) {
+                total += integer;
+            }
+        }
+        return total;
+    }
+
 
 
 }
