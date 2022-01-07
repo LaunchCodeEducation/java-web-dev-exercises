f870126ed4071e84e2e3249276d9dd5149e9a4bb
Sergio Estrada <estrada.serg@gmail.com>
2022-01-07 03:18:08 -0600

    finished studio 1

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,26 @@
+package org.launchcode.java.studios.areaofacircle;
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+
+        double radiusInput;
+
+        do{
+            if(!input.hasNextInt() && !input.hasNextDouble()){
+                System.out.println("Invalid input!");
+                System.exit(0);
+            }
+
+            radiusInput = input.nextDouble();
+
+            if(radiusInput <= 0){
+                System.out.println("Enter a number above 0: ");
+            }
+        } while(radiusInput <= 0);
+
+        System.out.println("The area of a circle with a radius of " + radiusInput + " is: " + Circle.getArea(radiusInput));
+    }
+}
---
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,7 @@
+package org.launchcode.java.studios.areaofacircle;
+
+public class Circle {
+    public static double getArea(Double radius){
+        return 3.14 * radius * radius;
+    }
+}
