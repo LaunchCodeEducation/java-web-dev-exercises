647117f8e1966026650c1bc0b25bc45b0a1c4972
Sergio Estrada <estrada.serg@gmail.com>
2022-01-16 10:09:14 -0600

    finished excercises for class 4

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# About this Repository
+-# About this Repository
 
 This repo contains starter code for the in-book examples for LaunchCode's
 [Java Web Development](https://education.launchcode.org/java-web-development/index.html)
--- src/org/launchcode/java/demos/lsn4classes2/Student.java
+++ src/org/launchcode/java/demos/lsn4classes2/Student.java
@@ -30,17 +30,44 @@ public class Student {
 
 
      //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
-
-    // TODO: Complete the addGrade method.
+    public String getGradeLevel() {
+        if(this.numberOfCredits >= 90){
+            return "Senior";
+        } else if(this.numberOfCredits >= 60){
+            return "Junior";
+        } else if(this.numberOfCredits >= 30){
+            return "Sophomore";
+        } else {
+            return "Freshman";
+        }
+    }
+   // TODO: Complete the addGrade method.
     public void addGrade(int courseCredits, double grade) {
-        // Update the appropriate fields: numberOfCredits, gpa
+        this.gpa = (courseCredits * grade) + (this.gpa * this.numberOfCredits);
+        this.numberOfCredits += courseCredits;
+
+        this.gpa = this.gpa / this.numberOfCredits;
     }
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
     //  than just the class fields.
+    public String toString(){
+        return "Name: " + name + "(GPA: " + Math.round(gpa*100.0)/100.0 + ", Credits: " + numberOfCredits + ", Rank: " + this.getGradeLevel() + ")";
+    }
+
+    public boolean equals(Object toBeCompared){
+        if(toBeCompared == null){
+            return false;
+        }
+
+        if(toBeCompared.getClass() != getClass()){
+            return false;
+        }
+
+        Student theStudent = (Student) toBeCompared;
+
+        return theStudent.getStudentId() == getStudentId();
+    }
 
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Student objects equal.
@@ -78,12 +105,17 @@ public class Student {
     }
 
     public static void main(String[] args) {
-        Student sally = new Student("Sally",1,1,4.0);
+        Student sally = new Student("Sally",1,0,4.0);
         System.out.println("The Student class works! " + sally.getName() + " is a student!");
+
+       Student sallyTwo = new Student("SallyTwo", 1, 0, 4.0);
+
+       System.out.println(sally.equals(sallyTwo));
+       /* System.out.println(sally);
+        sally.addGrade(3, 4.0);
         System.out.println(sally);
-        sally.addGrade(12, 3.5);
+        sally.addGrade(4, 3.0);
         System.out.println(sally);
-        sally.addGrade(25, 3.8);
-        System.out.println(sally);
+        */
     }
 }
