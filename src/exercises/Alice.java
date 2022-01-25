cdd22800bd5bd49531edd72f9cefda179dc7dcad
Iurii Rudov <iuriirudov@gmail.com>
2022-01-25 16:43:38 -0500

    Exercises Chapter 2 Part 4

---
+++ src/exercises/Alice.java
@@ -0,0 +1,16 @@
+package exercises;
+import java.util.Scanner;
+
+public class Alice {
+    public static void main(String[] args) {
+        String sentence = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Type your search word and I will check if it is within the sting I have: ");
+        String request = input.nextLine();
+
+        boolean result = sentence.toLowerCase().contains(request.toLowerCase());
+        System.out.println(result);
+    }
+}
