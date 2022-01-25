28da3bbb57672f99363401ee7ab7c303677f8377
Iurii Rudov <iuriirudov@gmail.com>
2022-01-25 16:14:16 -0500

    Exercises Chapter 2 Part 2.

--- src/exercises/Alice.java
+++
@@ -1,7 +0,0 @@
-package exercises;
-
-public class Alice {
-    public static void main(String[] args) {
-        System.out.println("Hello!");
-    }
-}
---
+++ src/exercises/Rectangle.java
@@ -0,0 +1,20 @@
+package exercises;
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner inputLength = new Scanner(System.in);
+
+        System.out.print("Type the length of the rectangle: ");
+        double length = inputLength.nextDouble();
+
+        Scanner inputWidth = new Scanner(System.in);
+
+        System.out.print("Type the width of the rectangle: ");
+        double width = inputWidth.nextDouble();
+
+        double total = length * width;
+
+        System.out.println("The area of the rectangle is: " + total);
+    }
+}
