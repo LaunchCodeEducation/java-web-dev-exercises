626ed4b40146b8e607c484863e0d53d32b6c535a
Kieran <kieranbarry51@gmail.com>
2022-04-14 20:02:27 -0500

    -bonus mission 1 & 2

--- src/studio3/CountingCharacters.java
+++ src/studio3/CountingCharacters.java
@@ -2,15 +2,23 @@ package studio3;
 
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Scanner;
 
 public class CountingCharacters {
 
     public static void main(String[] args) {
 
-        String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. " +
-                "So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero." +
-                " Once you’ve done that, it’s pretty straightforward from there.";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string of characters: ");
+
+        String quote = input.nextLine().toLowerCase();
+        input.close();
+
+       // String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. " +
+       //         "So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero." +
+        //" Once you’ve done that, it’s pretty straightforward from there.";
 
         char [] charactersInString = quote.toCharArray();
 
