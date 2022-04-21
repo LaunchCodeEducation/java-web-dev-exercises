3395cb644e4585691c250688d493d9b98caf62f5
jopamo <p@1g4.org>
2022-04-21 12:45:48 -0500

    MenuItem: add the getters and setters some need to be removed? (wip)

--- src/Studio4/MenuItem.java
+++ src/Studio4/MenuItem.java
@@ -1,22 +1,51 @@
 package Studio4;
 
 public class MenuItem {
+	private String name;
+	private String price;
+	private String description;
+	private String category;
+	private boolean ifNew;
 
-        private String name;
-        private String price;
-        private String description;
-        private String category;
-        private boolean ifNew;
+	public MenuItem(String name, String price, String description, String category, boolean ifNew) {
+		this.name= name;
+		this.price= price;
+		this.description= description;
+		this.category= category;
+		this.ifNew= ifNew;
+	}
 
-        public MenuItem(String name, String price, String description, String category,boolean ifNew){
-            this.name= name;
-            this.price= price;
-            this.description= description;
-            this.category= category;
-            this.ifNew= ifNew;
+	// getters
+	public double getName() {
+		return this.name;
+	}
 
-        }
+	public double getPrice() {
+		return this.price;
+	}
 
+	public String getDescription() {
+		return this.description;
+	}
 
+	public String getCategory() {
+		return this.category;
+	}
 
+	public boolean ifNew() {
+		return ifNew;
+	}
+
+	// setters
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setifNew(boolean ifNew) {
+		this.ifNew = ifNew;
+	}
 }
