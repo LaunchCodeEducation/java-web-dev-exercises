45e2265fbebd0e93e5be19098994e3e225cd2119
salev <salevillain@gmail.com>
2022-05-05 20:08:40 -0500

    implemented it

--- src/studio7/Main.java
+++ src/studio7/Main.java
@@ -5,8 +5,9 @@ public class Main {
 public static void main (String[] args){
 
     // TODO: Declare and initialize a CD and a DVD object.
-
+DVD Titanic = new DVD("Titanic", 1000000000, "Movie", "DVD");
     // TODO: Call each CD and DVD method to verify that they work as expected.
-
+System.out.println(Titanic.spinDisk());
+    System.out.println(Titanic.readData());
     }
 }
