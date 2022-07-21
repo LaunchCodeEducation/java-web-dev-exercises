c7e1bf9882bfea9cc756f507794fa7039ee374df
Nick Villone <n.villone8@gmail.com>
2022-07-20 20:30:10 -0400

    array exercise complete

---
+++ src/exercises/Array.java
@@ -0,0 +1,25 @@
+package exercises;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] integerArray = {1, 1, 2, 3, 5, 8};
+
+        for (int i : integerArray) {
+            if ( i%2 == 1 ) System.out.println(i);
+        }
+
+
+        String phrase = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+        String[] words = phrase.split(" ");
+        System.out.println(Arrays.toString(words));
+
+        String[] sentences = phrase.split("\\.");
+        System.out.println(Arrays.toString(sentences));
+
+    }
+
+}
+
+
