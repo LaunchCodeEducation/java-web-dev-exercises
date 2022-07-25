d2d54f4c09390f1b104f294d254ff1f39e2aa538
Nick Villone <n.villone8@gmail.com>
2022-07-25 18:50:13 -0400

    chapter 3 exercises completed

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,7 +1,16 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student nick = new Student("Nick", 12342, 1, 4.0);
+
+        System.out.println(nick.getName());
+        System.out.println(nick);
+
     }
+
+
 }
