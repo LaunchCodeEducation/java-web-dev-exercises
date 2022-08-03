ec8e9624f5184b57b95e499c979a9dec1bc13355
Nick Villone <n.villone8@gmail.com>
2022-08-02 21:52:10 -0400

    lesson 5 exercises complete

--- src/org/launchcode/java/demos/lsn5unittesting/main/Car.java
+++ src/org/launchcode/java/demos/lsn5unittesting/main/Car.java
@@ -39,8 +39,11 @@ public class Car {
     }
 
     public void setGasTankSize(int gasTankSize) {
-        this.gasTankSize = gasTankSize;
-    }
+            if (gasTankLevel > this.getGasTankSize()) {
+                throw new IllegalArgumentException("Can't exceed tank size");
+            }
+            this.gasTankLevel = gasTankLevel;
+        }
 
     public double getGasTankLevel() {
         return gasTankLevel;
@@ -83,6 +86,10 @@ public class Car {
         double gallonsUsed = milesAbleToTravel / this.milesPerGallon;
         this.gasTankLevel = this.gasTankLevel - gallonsUsed;
         this.odometer += milesAbleToTravel;
+    }
+
+    public void addGas(double gas) {
+        this.setGasTankLevel(gas + this.getGasTankLevel());
     }
 
 }
