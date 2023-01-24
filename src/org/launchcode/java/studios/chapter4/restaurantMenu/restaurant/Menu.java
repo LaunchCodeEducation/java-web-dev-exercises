2e4a30096a23a89730e60acbca11c121d704269f
Anthony Sega <94858432+Antsega@users.noreply.github.com>
2023-01-24 16:05:14 -0600

    Completed through studio 5

--- src/org/launchcode/java/studios/chapter4/restaurantMenu/restaurant/Menu.java
+++ src/org/launchcode/java/studios/chapter4/restaurantMenu/restaurant/Menu.java
@@ -4,29 +4,76 @@ import java.util.*;
 
 
 public class Menu {
-    public static void main (String[] args) {
-        ArrayList<MenuItem> menuList = new ArrayList<>();
+    private ArrayList<MenuItem> entres = new ArrayList<>();
+    private ArrayList<MenuItem> drinks = new ArrayList<>();
+    private ArrayList<MenuItem> dressings = new ArrayList<>();
 
-        // create instances of MenuItem
+    public void entres() {
+        // item 1
         MenuItem item1 = new MenuItem();
         item1.setItemName("Cheese Pizza");
         item1.setItemPrice(10.00);
+        entres.add(item1);
+        // item 2
         MenuItem item2 = new MenuItem();
         item2.setItemName("Bacon Topped Pizza");
         item2.setItemPrice(12.50);
-        MenuItem item3 = new MenuItem();
-        item3.setItemName("Side of Ranch");
-        item3.setItemPrice(0.75);
-
-        // add items to AL
-        menuList.add(item1);
-        menuList.add(item2);
-        menuList.add(item3);
-
-        // print items
-        System.out.println("Our menu contains\n- - - - - - - - - - ");
-        for (int i = 0; i < menuList.size(); i++) {
-            System.out.println("Item " + (i + 1) + " is : " + menuList.get(i).getItemName() + " $" + menuList.get(i).getItemprice());
+        entres.add(item2);
+    }
+    public String getEntres() {
+        String entresMenu = "";
+        for (int i = 0; i < entres.size(); i++) {
+            if (entres.get(i).equals(entres.get(entres.size() - 1))) {
+                entresMenu += (i + 1) + ": " + entres.get(i).getItemName() + " $" + entres.get(i).getItemprice() + "\t*Newest Entre*\n";
+            }
+            else entresMenu += (i + 1) + ": " + entres.get(i).getItemName() + " $" + entres.get(i).getItemprice() + "\n";
         }
+        return entresMenu;
+    }
+
+    public void drinks() {
+        // item 1
+        MenuItem sprite = new MenuItem();
+        sprite.setItemName("Sprite");
+        sprite.setItemPrice(1.00);
+        drinks.add(sprite);
+        // item 2
+        MenuItem coke = new MenuItem();
+        coke.setItemName("Coke");
+        coke.setItemPrice(1.00);
+        drinks.add(coke);
+    }
+    public String getDrinks() {
+        String drinkMenu = "";
+        for (int i = 0; i < drinks.size(); i++) {
+            if (drinks.get(i).equals(drinks.get(drinks.size() - 1))) {
+                drinkMenu += (i + 1) + ": " + drinks.get(i).getItemName() + " $" + drinks.get(i).getItemprice() + "\t*Newest Drink*\n";
+            }
+            else drinkMenu += (i + 1) + ": " + drinks.get(i).getItemName() + " $" + drinks.get(i).getItemprice() + "\n";
+        }
+        return drinkMenu;
+    }
+
+    public void dressing() {
+        // item 1
+        MenuItem ranch = new MenuItem();
+        ranch.setItemName("Side of Ranch");
+        ranch.setItemPrice(0.75);
+        dressings.add(ranch);
+        // item 2
+        MenuItem blueCheese = new MenuItem();
+        blueCheese.setItemName("Side of Blue Cheese");
+        blueCheese.setItemPrice(0.75);
+        dressings.add(blueCheese);
+    }
+    public String getDressings(){
+        String dressingMenu = "";
+        for (int i = 0; i < dressings.size(); i++) {
+            if (dressings.get(i).equals(dressings.get(dressings.size() - 1))) {
+                dressingMenu += (i + 1) + ": " + dressings.get(i).getItemName() + " $" + dressings.get(i).getItemprice() + "\t*Newest Dressing*\n";
+            }
+            else dressingMenu += (i + 1) + ": " + dressings.get(i).getItemName() + " $" + dressings.get(i).getItemprice() + "\n";
+        }
+        return dressingMenu;
     }
 }
