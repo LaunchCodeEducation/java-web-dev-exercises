e6b9ea6b29fb1c6bbd1216a4498b034081aa76be
Anthony Sega <94858432+Antsega@users.noreply.github.com>
2023-01-25 12:21:05 -0600

    Completed through excerise 6

--- src/org/launchcode/java/demos/lsn5unittesting/main/Car.java
+++ src/org/launchcode/java/demos/lsn5unittesting/main/Car.java
@@ -47,6 +47,9 @@ public class Car {
     }
 
     public void setGasTankLevel(double gasTankLevel) {
+        if (gasTankLevel > this.getGasTankSize()) {
+            throw new IllegalArgumentException("Can't exceed tank size");
+        }
         this.gasTankLevel = gasTankLevel;
     }
 
@@ -83,6 +86,10 @@ public class Car {
         double gallonsUsed = milesAbleToTravel / this.milesPerGallon;
         this.gasTankLevel = this.gasTankLevel - gallonsUsed;
         this.odometer += milesAbleToTravel;
+    }
+
+    public void addGas(double gas) {
+        this.setGasTankLevel(gas + this.getGasTankLevel());
     }
 
 }
