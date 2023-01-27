0a74c655d470a56a012ccd005743e4d5a103a86a
Anthony Sega <astrowelding@gmail.com>
2023-01-27 11:00:13 -0600

    starter code for studio 8

---
+++ src/org/launchcode/java/studios/chapter8/studio/DVD.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.studios.chapter8.studio;
+
+public class DVD {
+
+    // TODO: Implement your custom interface.
+
+    // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
+    //  need to be declared separately.
+
+}
---
+++ src/org/launchcode/java/studios/chapter8/studio/Main.java
@@ -0,0 +1,12 @@
+package org.launchcode.java.studios.chapter8.studio;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        // TODO: Declare and initialize a CD and a DVD object.
+
+        // TODO: Call each CD and DVD method to verify that they work as expected.
+
+    }
+}
